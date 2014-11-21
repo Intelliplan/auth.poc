299300ae6b6e714b6d8fa1d1836d87d57cf015bc
Henrik Feldt <henrik@haf.se>
2014-11-21 13:59:32 +0100

    trying different config

--- idp.host/Program.fs
+++ idp.host/Program.fs
@@ -4,6 +4,7 @@ open System
 
 open Thinktecture.IdentityServer.Core.Logging
 
+open Owin
 open Microsoft.Owin
 open Microsoft.Owin.Hosting
 
@@ -14,6 +15,11 @@ let main argv =
   Console.Title <- "Intelliplan IdP PoC"
   LogProvider.SetCurrentLogProvider(new DiagnosticsTraceLogProvider())
 
-  let _ = WebApp.Start<IntelliplanPoC>("http://localhost:1234/core")
+  let start : string -> (IAppBuilder -> unit) -> IDisposable =
+    fun url f_builder ->
+      let options = StartOptions url
+      options.AppStartup <- typeof<IntelliplanPoC>.AssemblyQualifiedName
+      WebApp.Start(options, Action<_>(f_builder))
+  use app = start "https://localhost:9443/core" <| fun builder -> ()
   let _ = System.Console.ReadLine ()
   0
